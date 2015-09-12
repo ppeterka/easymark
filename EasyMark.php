b577c98b9bfb59c0a53eb8b6a18f4ca03760c992
ppeterka <p.peter.laszlo@gmail.com>
2015-09-12 16:34:21 +0200

    Initial commit:  * Parsedown library 1.5.0 included  * content display does work (yay!)  * adding section does work

---
+++ EasyMark.php
@@ -0,0 +1,69 @@
+<?php
+
+defined('is_running') or die('Not an entry point...');
+
+include $addonPathCode."lib/parsedown/Parsedown.php";
+
+
+
+class EasyMark
+{
+	public static $sectionType = 'easymark_section';
+
+	function SectionTypes( $section_types) {
+		$section_types[self::$sectionType] = array();
+		$section_types[self::$sectionType]['label'] = 'EasyMark';
+		return $section_types;
+	}
+
+	function SectionToContent($section_data) {
+		if( $section_data['type'] != self::$sectionType ) {
+			return $section_data;
+		}
+		$section_data['content']=(new ParseDown())->text($section_data['content']);
+		return $section_data;
+	}
+	function DefaultContent($default_content,$type) {
+		
+		if( $type != self::$sectionType ) {
+			return $default_content;
+		}
+
+		$section = array();
+		$section['content'] = "Hello **MarkDown** _world_!!!";
+		$section['uniqid'] = "em-" . crc32(uniqid("",true));
+		return $section;
+		
+		
+		
+	}
+	function SaveSection($return,$section,$type) {
+		if( $type != self::$sectionType ) {
+		  return $return;
+		}
+		global $page;
+		$content =& $_POST['gpcontent'];
+		$page->file_sections[$section]['content'] = $content;
+		return true;
+		
+	
+	}
+	function GenerateContent_Admin() {
+		global $addonFolderName, $page;
+		static $done = false;
+		if ($done || !common::LoggedIn()) { return; }
+		$done = true;
+		
+	}
+	function InlineEdit_Scripts($scripts,$type) {
+		if( $type != self::$sectionType ) {
+		  return $scripts;
+		}
+		
+		$scripts = "Hello!";
+		return $scripts;
+	}
+
+}
+
+
